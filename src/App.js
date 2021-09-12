f994bb9658b44f2aed46b3daf58e67df76833f71
a-u-z <qinemperor0206@gmail.com>
2021-09-12 12:23:36 +0800

    initialization

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="text-yellow-400 ">
+          Edit <code className="">src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
