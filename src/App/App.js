da4af781cbc73b37c888fa716a21cb01818d7ec6
Lindsay <lliny0214tw@icloud.com>
2021-09-22 15:04:35 +0800

    權限＆nav rwd

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { getMe } from '../api';
+import { selectUser } from '../features/userSlice';
 
 import AdminUpdatePage from '../pages/AdminUpdatePage';
 import HomePage from '../pages/HomePage';
@@ -26,50 +28,72 @@ import UserPasswordPage from '../pages/UserEditPage/UserPasswordPage';
 import UserEmailPage from '../pages/UserEditPage/UserEmailPage';
 import UserCreditCardPage from '../pages/UserEditPage/UserCreditCardPage';
 // import Hamburger from '../components/Hamburger';
+import AdminNavbar from '../components/AdminNavbar';
 
 function App() {
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    getMe().then((response) => {
-      if (response.ok) {
-        setUser(response.data);
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   getMe().then((response) => {
+  //     if (response.ok) {
+  //       setUser(response.data);
+  //     }
+  //   });
+  // }, []);
+  const user = useSelector(selectUser);
 
   return (
     <div className="h-auto bg-yellow-lightYellow">
       <Router>
-        <Navbar />
-        {/* <Hamburger /> */}
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/menu" component={MenuPage} />
-          {/* <Route path="*" component={NoFoundPage} /> */}
-          {/* user */}
-          <Route path="/login" component={LoginPage} />
-          <Route path="/register" component={RegisterPage} />
-          <Route path="/no-permission" component={NoPermissionPage} />
-          <Route path="/user-update" component={UserUpdatePage} />
-          <Route path="/user-edit" component={UserEditPage} />
-          <Route path="/user-edit-password" component={UserPasswordPage} />
-          <Route path="/user-edit-email" component={UserEmailPage} />
-          <Route path="/user-edit-creditcard" component={UserCreditCardPage} />
-          <Route path="/credit-card-start" component={CreditCardStartPage} />
-          <Route path="/credit-card-delete" component={CreditCardDeletePage} />
-          <Route path="/credit-card-update" component={CreditCardUpdatePage} />
-          {/* order */}
-          <Route path="/order" component={OrderPage} />
-          <Route path="/orders" component={OrdersPage} />
-          <Route path="/order-pay" component={OrderPayPage} />
-          <Route path="/order-info" component={OrderInfoPage} />
-          <Route path="/add-to-cart" component={AddToCartPage} />
-          <Route path="/order-check" component={OrderCheckPage} />
-          {/* admin */}
-          <Route path="/admin-update" component={AdminUpdatePage} />
-          <Route path="/admin-edit" component={AdminEditPage} />
-        </Switch>
+        {console.log('首頁：', user)}
+        {user && user.role === 'admin' ? (
+          <>
+            <AdminNavbar />
+            <Route path="/admin-update" component={AdminUpdatePage} />
+            <Route path="/admin-edit" component={AdminEditPage} />
+          </>
+        ) : (
+          <>
+            <Navbar />
+            {/* <Hamburger /> */}
+            <Switch>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/menu" component={MenuPage} />
+              {/* <Route path="*" component={NoFoundPage} /> */}
+              {/* user */}
+              <Route path="/login" component={LoginPage} />
+              <Route path="/register" component={RegisterPage} />
+              <Route path="/no-permission" component={NoPermissionPage} />
+              <Route path="/user-update" component={UserUpdatePage} />
+              <Route path="/user-edit" component={UserEditPage} />
+              <Route path="/user-edit-password" component={UserPasswordPage} />
+              <Route path="/user-edit-email" component={UserEmailPage} />
+              <Route
+                path="/user-edit-creditcard"
+                component={UserCreditCardPage}
+              />
+              <Route
+                path="/credit-card-start"
+                component={CreditCardStartPage}
+              />
+              <Route
+                path="/credit-card-delete"
+                component={CreditCardDeletePage}
+              />
+              <Route
+                path="/credit-card-update"
+                component={CreditCardUpdatePage}
+              />
+              {/* order */}
+              <Route path="/order" component={OrderPage} />
+              <Route path="/orders" component={OrdersPage} />
+              <Route path="/order-pay" component={OrderPayPage} />
+              <Route path="/order-info" component={OrderInfoPage} />
+              <Route path="/add-to-cart" component={AddToCartPage} />
+              <Route path="/order-check" component={OrderCheckPage} />
+            </Switch>
+          </>
+        )}
         <Footer />
       </Router>
     </div>
