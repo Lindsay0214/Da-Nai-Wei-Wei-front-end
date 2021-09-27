ff2d3ab844825b492ab5557e7f9bf332f12b3aab
Lindsay <lliny0214tw@icloud.com>
2021-09-28 00:44:30 +0800

    [修改/選單]使用者狀態

--- src/App/App.js
+++ src/App/App.js
@@ -45,7 +45,6 @@ import UserEmailPage from '../pages/UserEditPage/UserEmailPage';
 import UserCreditCardPage from '../pages/UserEditPage/UserCreditCardPage';
 import UserUploadImage from '../pages/UserUploadImagePage';
 
-import Hamburger from '../components/Hamburger';
 import AdminNavbar from '../components/AdminNavbar';
 
 function App() {
@@ -87,7 +86,7 @@ function App() {
             <Route path="/menu/:id" component={MenuPage} />
             {/* <Route path="*" component={NoFoundPage} /> */}
             {/* user */}
-            <Route path="/login" component={Hamburger} />
+            <Route path="/login" component={LoginPage} />
             <Route path="/register" component={RegisterPage} />
             <Route path="/user-update" component={UserUpdatePage} />
             <Route path="/user-edit" component={UserEditPage} />
--- src/components/Ham.js
+++ src/components/Ham.js
@@ -1,10 +1,21 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { Menu, Transition } from '@headlessui/react';
 import { Fragment } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
+import { Link, useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectUser, logout } from '../features/userSlice';
 
 export default function Ham() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(logout());
+    history.push('/');
+  };
+
   return (
     <div className="fixed top-0 w-full text-right">
       <Menu
@@ -28,7 +39,7 @@ export default function Ham() {
           leaveTo="transform opacity-0 scale-95"
         >
           {/* Menu */}
-          <Menu.Items className="absolute w-40 origin-top-right rounded-lg top-10 right-12 bg-yellow-deepYellow focus:outline-none">
+          <Menu.Items className="absolute w-40 origin-top-right rounded-lg top-12 right-14 bg-yellow-deepYellow focus:outline-none">
             <div className="px-2 py-3">
               <Menu.Item>
                 <Link
@@ -54,30 +65,37 @@ export default function Ham() {
                   訂單
                 </Link>
               </Menu.Item>
-              <Menu.Item>
-                <Link
-                  className="items-center inline-block px-12 py-2 mt-2 text-base leading-snug tracking-widest text-white rounded-lg bg-yellow-deepYellow hover:hover"
-                  to="/login"
-                >
-                  登入
-                </Link>
-              </Menu.Item>
-              <Menu.Item>
-                <Link
-                  className="items-center inline-block px-12 py-2 mt-2 text-base leading-snug tracking-widest text-white rounded-lg bg-yellow-deepYellow hover:hover"
-                  to="/register"
-                >
-                  註冊
-                </Link>
-              </Menu.Item>
-              <Menu.Item>
-                <Link
-                  className="items-center inline-block px-12 py-2 mt-2 text-base leading-snug tracking-widest text-white rounded-lg bg-yellow-deepYellow hover:hover"
-                  to="/logout"
-                >
-                  登出
-                </Link>
-              </Menu.Item>
+              {!user && (
+                <Menu.Item>
+                  <Link
+                    className="items-center inline-block px-12 py-2 mt-2 text-base leading-snug tracking-widest text-white rounded-lg bg-yellow-deepYellow hover:hover"
+                    to="/login"
+                  >
+                    登入
+                  </Link>
+                </Menu.Item>
+              )}
+              {!user && (
+                <Menu.Item>
+                  <Link
+                    className="items-center inline-block px-12 py-2 mt-2 text-base leading-snug tracking-widest text-white rounded-lg bg-yellow-deepYellow hover:hover"
+                    to="/register"
+                  >
+                    註冊
+                  </Link>
+                </Menu.Item>
+              )}
+              {user && (
+                <Menu.Item>
+                  <Link
+                    className="items-center inline-block px-12 py-2 mt-2 text-base leading-snug tracking-widest text-white rounded-lg bg-yellow-deepYellow hover:hover"
+                    to="/logout"
+                    onClick={(e) => handleLogout(e)}
+                  >
+                    登出
+                  </Link>
+                </Menu.Item>
+              )}
             </div>
           </Menu.Items>
         </Transition>
