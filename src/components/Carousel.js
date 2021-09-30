386fc26a4e1eb009d2cac3f227c548fcb8d50bc4
Lindsay <lliny0214tw@icloud.com>
2021-09-30 19:08:22 +0800

    [挽救/全域]丟失的樣式們

---
+++ src/components/Carousel.js
@@ -0,0 +1,54 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useState, useEffect } from 'react';
+import carousel1 from '../image/carousel1.png';
+import carousel2 from '../image/carousel2.png';
+import carousel3 from '../image/carousel3.png';
+import carousel4 from '../image/carousel4.png';
+import carousel5 from '../image/carousel5.png';
+import carousel6 from '../image/carousel6.png';
+
+const Carousel = ({
+  images = [carousel1, carousel2, carousel3, carousel4, carousel5, carousel6],
+  autoPlay = true,
+  autoPlayTime = 6000,
+  children,
+  ...props
+}) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  function nextSlide(slideIndex = currentSlide + 1) {
+    const newSlideIndex = slideIndex >= images.length ? 0 : slideIndex;
+
+    setCurrentSlide(newSlideIndex);
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      nextSlide();
+    }, autoPlayTime);
+
+    return () => clearTimeout(timer);
+  }, [currentSlide]);
+
+  return (
+    <div
+      className="m-auto md:rounded-lg md:w-160 lg:w-2/3 lg:mt-10 h-1/2 wrapper"
+      {...props}
+    >
+      {images.map((imageUrl, index) => (
+        <div
+          className="slide"
+          key={index}
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+            marginLeft: index === 0 ? `-${currentSlide * 100}%` : undefined
+          }}
+        ></div>
+      ))}
+      <div className="childrenWrapper">{children}</div>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const NavbarButton = ({ data }) => {
   return (
     <li className="nav-item">
       <Link
-        className="items-center hidden px-8 text-lg leading-snug tracking-wide text-black py-9 hover:bg-yellow-deepYellow hover:text-white lg:inline-block lg:mt-0"
+        className="items-center hidden text-lg leading-snug tracking-wide text-black md:px-2 lg:px-8 py-9 hover:bg-yellow-deepYellow hover:text-white md:inline-block md:mt-0"
         to={data.url}
       >
         {data.name}
@@ -17,7 +17,6 @@ const NavbarButton = ({ data }) => {
 };
 const Navbar = () => {
   const data1 = { name: '附近店家', url: '/' };
-  const data2 = { name: '所有店家', url: '/' };
   const data3 = { name: '購物車', url: '/order' };
   const data4 = { name: '歷史訂單', url: '/orders' };
 
@@ -31,7 +30,7 @@ const Navbar = () => {
   };
   return (
     <>
-      <nav className="relative flex items-center h-24 py-16 md:py-20 lg:py-0 bg-yellow-lightYellow">
+      <nav className="relative flex items-center h-24 py-16 md:py-20 lg:py-0 bg-yellow-lightYellow ">
         {/* after:border lg:after:border-none */}
         <div className="container flex items-center content-around justify-between mx-auto space-x-5">
           {/* logo */}
@@ -48,14 +47,13 @@ const Navbar = () => {
           {/* list */}
           <ul className="flex -ml-16 space-x-8 md:space-x-0">
             <NavbarButton data={data1}></NavbarButton>
-            <NavbarButton data={data2}></NavbarButton>
             <NavbarButton data={data3}></NavbarButton>
             <NavbarButton data={data4}></NavbarButton>
           </ul>
           <div className="hidden mx-4 nav-item sm:flex">
             {!user && (
               <Link
-                className="items-center hidden px-12 text-lg leading-snug tracking-wide text-black lg:inline-block py-9 bg-yellow-default hover:bg-yellow-deepYellow hover:text-white"
+                className="items-center hidden px-12 text-lg leading-snug tracking-wide text-black md:inline-block py-9 bg-yellow-default hover:bg-yellow-deepYellow hover:text-white"
                 to="/register"
               >
                 註冊
@@ -63,7 +61,7 @@ const Navbar = () => {
             )}
             {!user && (
               <Link
-                className="items-center hidden px-12 text-lg leading-snug tracking-wide text-black lg:inline-block py-9 bg-yellow-default hover:bg-yellow-deepYellow hover:text-white"
+                className="items-center hidden px-12 text-lg leading-snug tracking-wide text-black md:inline-block py-9 bg-yellow-default hover:bg-yellow-deepYellow hover:text-white"
                 to="/login"
               >
                 登入
@@ -71,11 +69,13 @@ const Navbar = () => {
             )}
             {user && (
               <>
-                <p className="items-center hidden leading-snug tracking-wide text-black lg:inline-block py-9 bg-yellow-default">
-                  <Link to="/user-update">{user.email} info</Link>
+                <p className="items-center hidden leading-snug tracking-wide text-black md:inline-block py-9 bg-yellow-default hover:bg-yellow-deepYellow hover:text-white">
+                  <Link to="/user-update" className="px-4 ">
+                    Hi，{user.nickname}
+                  </Link>
                 </p>
                 <Link
-                  className="items-center hidden px-12 text-lg leading-snug tracking-wide text-black lg:inline-block py-9 bg-yellow-default hover:bg-yellow-deepYellow hover:text-white"
+                  className="items-center hidden px-12 text-lg leading-snug tracking-wide text-black md:inline-block py-9 bg-yellow-default hover:bg-yellow-deepYellow hover:text-white"
                   to="/logout"
                   onClick={(e) => handleLogout(e)}
                 >
