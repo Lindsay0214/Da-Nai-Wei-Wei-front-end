e8ce424f97b2f10d389af03e31aa9ca237377e29
a_u_z <77137373+a-u-z@users.noreply.github.com>
2021-09-30 20:47:53 +0800

    [fix/版面]修改 navbar email 會過長問題、首頁店名過長問題

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useHistory } from 'react-router';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { login } from '../../features/userSlice';
+import { login, getMe } from '../../features/userSlice';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
