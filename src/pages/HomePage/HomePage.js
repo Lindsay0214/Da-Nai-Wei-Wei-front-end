8883c64b7551437f633356b47abae2f9327d6d46
Jason <jason80826@gmail.com>
2021-10-02 16:54:54 +0800

    [修改/錯誤] 修改錯誤

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,23 +1,43 @@
 /* eslint-disable react/destructuring-assignment */
 import React, { useState } from 'react';
-import { FaStar } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+import { FaStar, FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import GoogleMap from '../../components/GoogleMap';
 import Carousel from '../../components/Carousel';
 
 const HomePageInput = ({ data, setSearchShop }) => {
   return (
-    <input
-      placeholder={data}
-      className="w-4/12 h-8 mx-1 text-xs text-center rounded md:mx-2 lg:w-60 md:w-44 lg:h-10 lg:rounded-md md:text-base"
-      onChange={async (e) => {
-        if (e.target.value === '') {
-          setSearchShop(['嵐', '麻古', '迷']);
-          return;
-        }
-        setSearchShop([e.target.value]);
-      }}
-    ></input>
+    <>
+      <div className="w-full">
+        <div className="flex items-center bg-white rounded-full shadow-xl">
+          <input
+            className="w-full px-6 py-4 leading-tight text-gray-700 rounded-l-full focus:outline-none"
+            id="search"
+            type="text"
+            placeholder={data}
+            onChange={async (e) => {
+              if (e.target.value === '') {
+                setSearchShop(['嵐', '麻古', '迷']);
+                return;
+              }
+              if (e.target.value.length > 5) {
+                toast.error('哎唷！太長了', {
+                  position: toast.POSITION.TOP_CENTER,
+                  theme: 'colored'
+                });
+              }
+              setSearchShop([e.target.value]);
+            }}
+          />
+          <div className="p-4">
+            <button className="flex items-center justify-center w-12 h-12 p-2 text-white duration-500 ease-in-out rounded-full bg-yellow-deepYellow hover:hover focus:outline-none">
+              <FaSearch></FaSearch>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 const HomePageShop = ({ shop }) => {
@@ -62,25 +82,15 @@ const HomePage = () => {
     <>
       <GoogleMap handleChange={handleChange} searchShop={searchShop} />
       <Carousel />
-      <div className="flex items-center mx-auto mt-16 rounded-lg md:w-160 w-72 h-14 bg-yellow-deepYellow lg:w-234 lg:rounded-lg lg:h-24">
-        <div className="flex justify-around mx-auto lg:items-center md:justify-around w-72 h-7 lg:w-full lg:h-14">
-          <HomePageInput
-            key="findBrand"
-            data="找品牌..."
-            setSearchShop={setSearchShop}
-          ></HomePageInput>
-          {/* <HomePageInput
-            key="findCategory"
-            data="找種類..."
-            setSearchShop={setSearchShop}
-          ></HomePageInput>
-          <HomePageInput
-            key="findComment"
-            data="找評價..."
-            setSearchShop={setSearchShop}
-          ></HomePageInput> */}
-        </div>
+      <div className="flex items-center mx-auto mt-16 rounded-lg md:w-160 w-72 h-14 lg:w-234 lg:rounded-lg lg:h-24">
+        {/* <div className="flex justify-around mx-auto lg:items-center md:justify-around w-72 h-7 lg:w-full lg:h-14"> */}
+        <HomePageInput
+          key="findBrand"
+          data="找品牌"
+          setSearchShop={setSearchShop}
+        ></HomePageInput>
       </div>
+      {/* </div> */}
       <div className="mx-auto mt-10 lg:mt-20 w-min md:w-176 lg:w-270">
         <div className="flex flex-wrap h-auto m-auto md:space-x-12 lg:space-x-12 bg-yellow-light">
           <div></div>
