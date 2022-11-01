99287a9b718e38ea012f48840fef1179551a1813
Lindsay 🥳 <lindsay@LindsaydeMacBook-Pro.local>
2022-11-01 18:37:35 +0800

    add msw

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ import store from './store';
 
 const queryClient = new QueryClient();
 interceptor(store);
+
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line global-require
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
